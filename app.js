0693df452e72b5baae0b7c512e3ddbd4242ccd32
ivanceasa <ceaivan777@gmail.com>
2021-08-17 09:27:23 +0000

    Jest

---
+++ app.js
@@ -0,0 +1,36 @@
+// declaramos una funcion con el mismo nombre "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro){
+    // convertimos el valor a dolares
+    let valueInDollar = valueInEuro * 1.2;
+    // retornamos el valor
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar){
+    // convertimos el valor a Yens
+    let valueInYen = valueInDollar * (127.9/1.2);
+    // returnamos el valor
+    return valueInYen;
+}
+
+
+const fromYenToPound = function(valueInYen){
+    // convertimos el valor a Pounds
+    let valueInPounds = valueInYen * (0.8/127.9);
+    // returnamos el valor
+    return valueInPounds;
+}
+
+
+// esta es mi función que suma dos números
+const sum = (a,b) => {
+    return a + b
+}
+
+// solo un registro en consola para nosotros.
+console.log(sum(7,3))
+
+// exporta la función para usarla en otros archivos 
+// (similar a la palabra clave `export` cuando se usa webpack)
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
+
---
+++ test.js
@@ -0,0 +1,37 @@
+// importar la función sum del archivo app.js
+const { sum } = require('./app.js');
+
+// comienza tu primera prueba
+test('adds 14 + 9 to equal 23', () => {
+    //dentro de la prueba llamamos a nuestra función sum con 2 números
+    let total = sum(14, 9);
+
+    // esperamos que la suma de esos 2 números sea 23
+    expect(total).toBe(23);
+});
+
+test("One euro should be 1.206 dollars", function(){
+    // importo la funcion desde app.js
+    const { fromEuroToDollar } = require('./app.js')
+    
+    // hago mi comparacion (la prueba)
+    expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+})
+
+test("One dollar should be 106.583 yens", function(){
+    // importo la funcion desde app.js
+    const { fromDollarToYen } = require('./app.js')
+    
+    // hago mi comparacion (la prueba)
+    expect(fromDollarToYen(3.5)).toBe(373.0416666666667); //1 dolar son 106.583 yenes, entonces 3.5 dolares deberian ser = (3.5 * 106.583)
+})
+
+test("One yen should be 0.0062548866301 pounds", function(){
+    // importo la funcion desde app.js
+    const { fromYenToPound } = require('./app.js')
+    
+    // hago mi comparacion (la prueba)
+    expect(fromYenToPound(100)).toBe(0.6254886630179828); //1 yen son 0.0062548866301 libras, entonces 100 yenes deberian ser = (100 * 0.0062548866301)
+})
+
+
